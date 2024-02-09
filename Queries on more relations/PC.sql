7798cf0dbaa155a520c7c8046ce94cd75a17713e
Adriana Atanasova <97397646+atanasova07@users.noreply.github.com>
2024-02-09 22:17:50 +0200

    Added solutions to queries on more relations

---
+++ Queries on more relations/PC.sql
@@ -0,0 +1,54 @@
+USE PC;
+ 
+/*Напишете заявка, която извежда производителя и честотата на лаптопите с
+размер на диска поне 9 GB*/
+SELECT DISTINCT maker, speed
+FROM product JOIN laptop
+ON product.model = laptop.model
+WHERE hd >= 9 and type = 'Laptop';
+ 
+/*Напишете заявка, която извежда модел и цена на продуктите, произведени от
+производител с име B.*/
+SELECT product.model, laptop.price
+FROM product JOIN laptop
+ON product.model = laptop.model
+WHERE maker = 'B'
+UNION
+SELECT product.model, pc.price
+FROM product JOIN pc
+ON product.model = pc.model
+WHERE maker = 'B'
+UNION
+SELECT product.model, printer.price
+FROM product JOIN printer
+ON product.model = printer.model
+WHERE maker = 'B';
+ 
+/*Напишете заявка, която извежда производителите, които произвеждат лаптопи,
+но не произвеждат персонални компютри.*/
+SELECT maker
+FROM product JOIN laptop 
+ON product.model = laptop.model 
+EXCEPT
+SELECT maker
+FROM product JOIN pc
+ON product.model = pc.model;
+
+/*Напишете заявка, която извежда размерите на тези дискове, които се предлагат
+в поне два различни персонални компютъра (два компютъра с различен код).*/
+SELECT DISTINCT pc1.hd
+FROM  pc as pc1, pc as pc2
+WHERE pc1.hd = pc2.hd AND pc1.code != pc2.code; 
+
+/*Напишете заявка, която извежда двойките модели на персонални компютри,
+които имат еднаква честота и памет. Двойките трябва да се показват само по
+веднъж, например само (i, j), но не и (j, i).*/
+SELECT pc1.model, pc2.model
+FROM pc AS pc1, pc AS pc2
+WHERE pc1.speed = pc2.speed AND pc1.ram = pc2.ram AND pc1.model < pc2.model;
+
+/*Напишете заявка, която извежда производителите на поне два различни
+персонални компютъра с честота поне 400.*/
+SELECT DISTINCT maker
+FROM (product pr JOIN pc ON pr.model = pc.model) JOIN pc pc2 ON pr.model = pc2.model
+WHERE pc.speed >=400 AND pc2.speed >= 400 AND pc.code != pc2.code
---
+++ Queries on more relations/Ships.sql
@@ -0,0 +1,24 @@
+USE ships;
+
+/*Напишете заявка, която извежда името на корабите с водоизместимост над 50000.*/
+SELECT name
+FROM ships JOIN classes ON ships.class = classes.class
+WHERE displacement > 50000;
+
+/*Напишете заявка, която извежда имената, водоизместимостта и броя оръдия на
+всички кораби, участвали в битката при Guadalcanal.*/
+SELECT name, displacement, numguns
+FROM (outcomes JOIN ships ON ship=name ) JOIN classes ON ships.class = classes.class
+WHERE battle = 'Guadalcanal';
+
+/*Напишете заявка, която извежда имената на тези държави, които имат както
+бойни кораби, така и бойни крайцери*/
+SELECT DISTINCT c1.country
+FROM classes c1, classes c2
+WHERE c1.country = c2.country AND ((c1.type ='bb' AND c2.type ='bc') OR(c1.type ='bc' AND c2.type ='bb'));
+
+/*. Напишете заявка, която извежда имената на тези кораби, които са били
+повредени в една битка, но по-късно са участвали в друга битка.*/
+SELECT o1.ship
+FROM outcomes o1, outcomes o2, battles b1,  battles b2
+WHERE o1.battle = b1.name AND o2.battle = b2.name AND o1.result = 'damaged' AND b1.date<b2.date AND o1.ship = o2.ship;
