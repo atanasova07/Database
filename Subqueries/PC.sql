fc8d216f9edeb8b0445d93cfa333fab0e81267b4
Adriana Atanasova <97397646+atanasova07@users.noreply.github.com>
2024-02-12 12:38:29 +0200

    Added solutions on subqueries tasks

---
+++ Subqueries/PC.sql
@@ -0,0 +1,31 @@
+USE pc;
+
+/*Напишете заявка, която извежда производителите на персонални
+компютри с честота над 500*/
+SELECT DISTINCT maker
+FROM pc JOIN product ON pc.model = product.model
+WHERE speed > 500;
+
+/*Напишете заявка, която извежда код, модел и цена на принтерите с
+най-висока цена.*/
+SELECT code, model, price
+FROM printer
+WHERE price >= ALL (SELECT price FROM printer);
+
+/*Напишете заявка, която извежда лаптопите, чиято честота е по-ниска от
+честотата на всички персонални компютри.*/
+SELECT DISTINCT *
+FROM laptop
+WHERE speed < ALL (SELECT speed FROM pc);
+
+/*Напишете заявка, която извежда производителя на цветния принтер с
+най-ниска цена.*/
+SELECT maker
+FROM product JOIN printer ON product.model = printer.model
+WHERE printer.color = 'y' AND printer.price <= ALL (SELECT price FROM printer WHERE color = 'y');
+
+/*Напишете заявка, която извежда производителите на тези персонални
+компютри с най-малко RAM памет, които имат най-бързи процесори.*/
+SELECT DISTINCT maker
+FROM product JOIN pc ON product.model = pc.model
+WHERE ram <= ALL(SELECT ram FROM pc) OR speed >= ALL(SELECT speed FROM pc);
